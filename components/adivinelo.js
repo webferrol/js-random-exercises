44b860891d4d2ad913d73c0c02437a2b416dc6dd
webferrol <xurxo@webferrol.com>
2023-03-08 12:45:01 +0100

    Confetti

--- components/adivinelo.js
+++ components/adivinelo.js
@@ -1,10 +1,11 @@
 import { getRandom } from '../helpers/get-random'
+import Confetti from 'js-confetti'
 let tirada = 0
-let tiradas = []
-let numeroRandom = getRandom(101,1)
+const tiradas = []
+const numeroRandom = getRandom(101, 1)
 const tiradaInputUsuario = document.querySelector('#aleatorio')
 
-export function adivinelo ()  { 
+export function adivinelo () {
   console.log(numeroRandom)
   if (tirada === 10) return
   if (isNaN(tiradaInputUsuario.value)) return
@@ -13,21 +14,22 @@ export function adivinelo ()  {
 
   // lógica
   const numero = Number(tiradaInputUsuario.value)
-  
+
   if (numero === numeroRandom) {
-    tirada = 10    
-    document.getElementById('msg_aleatorio').textContent = `Ganaste!!!`
+    tirada = 10
+    document.getElementById('msg_aleatorio').textContent = 'Ganaste!!!'
+    const confetti = new Confetti()
+    confetti.addConfetti({
+      emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸']
+    })
     return
   } else if (numero > numeroRandom) {
     document.getElementById('msg_aleatorio').textContent = `El número es mas pequeño. Tirada ${tirada + 1}`
-
   } else {
     document.getElementById('msg_aleatorio').textContent = `El número es más grande. Tirada ${tirada + 1}`
   }
-  
-  
+
   document.getElementById('serie_aleatorio').textContent = tiradas.join()
- 
-  
-  tirada ++ 
+
+  tirada++
 }
